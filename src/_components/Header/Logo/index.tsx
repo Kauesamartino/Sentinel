953a7caf0f9cd072bc0b447ae9048f803b3f5e16
kauetrnt <rm559317@fiap.com.br>
2025-07-14 14:22:48 -0300

    Refactor code structure for improved readability and maintainability

--- src/_components/Header/Logo/index.tsx
+++ src/_components/Header/Logo/index.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
 import styles from './logo.module.scss'
+import logo from './logoccr.png'
 import Image from "next/image";
 
 export default function Logo() {
-    return(
-        <div className={styles.logo}> <p>Logo</p>
-            {/* <Image
-                src={''}
-                width={25}
-                height={25}
-                alt=""
-            /> */}
-            <Link href={'/'}></Link>
+    return (
+        <div className={styles.logo}>
+            <Link href={'/'}>
+                <Image
+                    src={logo}
+                    width={55}
+                    height={55}
+                    alt=""
+                />
+            </Link>
         </div>
     )
 }
--- src/_components/Header/index.tsx
+++ src/_components/Header/index.tsx
@@ -5,12 +5,12 @@ import styles from './header.module.scss'
 
 export default function Header() {
     return (
-        <div className={styles.grid}>
+        <header className={styles.grid}>
             <div className={styles.grid__left}>
                 <Logo />
                 <Menu />
             </div>
             <Perfil />
-        </div>
+        </header>
     )
 }
