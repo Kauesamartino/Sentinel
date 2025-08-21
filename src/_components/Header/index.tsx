c02b063ebd449a291f65d087661dad2a886299ba
kauetrnt <rm559317@fiap.com.br>
2025-08-21 13:45:20 -0300

    Remove Perfil component from Header for a cleaner layout

--- src/_components/Header/index.tsx
+++ src/_components/Header/index.tsx
@@ -1,6 +1,5 @@
 import Logo from './Logo'
 import { Menu } from './Menu'
-import { Perfil } from './Perfil'
 import styles from './header.module.scss'
 
 export default function Header() {
@@ -10,7 +9,6 @@ export default function Header() {
                 <Logo />
                 <Menu />
             </div>
-            <Perfil />
         </header>
     )
 }
