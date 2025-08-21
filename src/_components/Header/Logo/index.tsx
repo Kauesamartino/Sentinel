3d938a4984408fba70c2a71da15230bc6e8831d4
kauetrnt <rm559317@fiap.com.br>
2025-08-21 16:15:29 -0300

    style: update button styles for improved visual consistency and replace Image component with img tag in Logo

--- src/_components/Header/Logo/index.tsx
+++ src/_components/Header/Logo/index.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
-import styles from './logo.module.scss'
-import logo from './logoccr.png'
-import Image from "next/image";
+import styles from './logo.module.scss';
 
 export default function Logo() {
     return (
         <div className={styles.logo}>
             <Link href={'/'}>
-                <Image
-                    src={logo}
-                    width={55}
-                    height={55}
-                    alt=""
+                <img
+                    src="https://aemassets.grupoccr.com.br/content/dam/sites-modulares/pt/media/images/logos/motiva_logo_only.svg"
+                    width={150}
+                    height={80}
+                    alt="Logo Motiva"
+                    draggable={false}
+                    style={{ display: 'block' }}
                 />
             </Link>
         </div>
-    )
+    );
 }
