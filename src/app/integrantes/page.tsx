57bf436fbb5cc0b9c77c4e3346fc45242e1e7734
kauetrnt <rm559317@fiap.com.br>
2025-08-21 15:13:28 -0300

    feat: add integrantes page with styling and member list

---
+++ src/app/integrantes/page.tsx
@@ -0,0 +1,46 @@
+import styles from './integrantespage.module.scss';
+
+const lista = [
+	{
+		name: 'Kauê Samartino',
+		photo: '/kaue.jpg',
+		rm: '559317',
+		alt: 'foto de Kauê',
+	},
+	{
+		name: 'Davi Praxedes',
+		photo: '/xeds.jpg',
+		rm: '560719',
+		alt: 'foto de Davi',
+	},
+	{
+		name: 'João dos Santos',
+		photo: '/foto_joao.jpg',
+		rm: '560400',
+		alt: 'foto de João',
+	},
+];
+
+export default function IntegrantesPage() {
+	return (
+		<main className={styles.main}>
+			<div className={styles.container}>
+				<h1 className={styles.title}>Integrantes</h1>
+				<div className={styles.integrantesGrid}>
+					{lista.map((item) => (
+						<div className={styles.card} key={item.rm}>
+							<img
+								src={item.photo}
+								alt={item.alt}
+								className={styles.photo}
+								loading="lazy"
+							/>
+							<div className={styles.name}>{item.name}</div>
+							<div className={''}>RM: {item.rm}</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</main>
+	);
+}
