a57908883d3be28916b1995e84413a3015b9cd69
Kauesamartino <kaue_samartino@hotmail.com>
2025-11-07 20:05:50 -0300

    feat: update About component description for clarity and detail

--- src/_components/About/index.tsx
+++ src/_components/About/index.tsx
@@ -6,10 +6,20 @@ export default function About() {
             <div className={styles.apresentationText}>
                 <h2 className={styles.title}>Sobre o Sentinel</h2>
                 <p className={styles.description}>
-                    O Sentinel é uma iniciativa para melhorar a segurança e a eficiência na gestão de ocorrências. Um projeto
-                    dos alunos em conjunto com a FIAP e a Motiva, desenvolvido para facilitar o registro, acompanhamento e análise de eventos,
-                    promovendo uma resposta rápida e eficaz. Nossa plataforma oferece ferramentas intuitivas para usuários
-                    e administradores, garantindo transparência e colaboração em todas as etapas do processo.
+                    O Sentinel é uma iniciativa dos alunos em parceria com a FIAP e a Motiva. É uma solução de segurança inteligente 
+                    que utiliza visão computacional e inteligência artificial para detectar situações de risco em tempo real 
+                    dentro de trens e estações.
+                </p>
+                <br />
+                <p className={styles.description}>
+                    Ele funciona de forma integrada pelas câmeras de segurança, que identificam automaticamente comportamentos 
+                    suspeitos e enviam alertas para a central de monitoramento e também por meio de um chatbot no WhatsApp, 
+                    onde o usuário pode relatar ocorrências com texto, imagem, áudio ou vídeo.
+                </p>
+                <br />
+                <p className={styles.description}>
+                    Tudo isso acontece em uma plataforma simples, rápida e eficiente, que garante respostas imediatas e ações 
+                    precisas. O resultado é claro: menos violência, mais tranquilidade e viagens mais seguras.
                 </p>
             </div>
         </section>
